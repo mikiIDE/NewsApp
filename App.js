791d99aabf7bbd4c28727b29fa124400b0c396bf
mikiIDE <kinoko.miki@gmail.com>
2025-02-16 13:37:04 +0900

    add expo-constants

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import NewsPage from "./components/NewsPage";
+import React, { useState, useEffect } from "react"
 
 export default function App() {
+  const [news, setNews] = useState([]);
+  useEffect (() => {alert("useEffectが動きました！"),[]})
   return (
     <View style={styles.container}>
       <NewsPage imageuri="" title="" subtext="" />
