08632d0b3f55581a5b7cbe75e6e74c9a9d231c1b
mikiIDE <kinoko.miki@gmail.com>
2025-02-16 14:05:48 +0900

    add .env and newsAPIKey

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import NewsPage from "./components/NewsPage";
 import React, { useState, useEffect } from "react"
+import Constants from "expo-constants"
+import { NEWS_API_KEY } from '@env'
 
 export default function App() {
   const [news, setNews] = useState([]);
-  useEffect (() => {alert("useEffectが動きました！"),[]})
+  useEffect (() => { alert(NEWS_API_KEY) },[])
   return (
     <View style={styles.container}>
       <NewsPage imageuri="" title="" subtext="" />
